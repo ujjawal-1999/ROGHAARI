df22fb04b99b86c22cca4da2e2bc6af7f0278038
Ujjawal Jain <jainujjawal1999@gmail.com>
2020-03-25 14:16:40 +0530

    Init Commit

---
+++ rogi/js/canvas.js
@@ -0,0 +1,9 @@
+var c = document.querySelector("canvas");
+var ctx = c.getContext("2d");
+ctx.beginPath();
+ctx.moveTo(0, 0);
+ctx.lineTo(250, 0);
+ctx.lineTo(50, 130);
+ctx.lineTo(0, 80);
+ctx.fillStyle="#00605a"
+ctx.fill();
